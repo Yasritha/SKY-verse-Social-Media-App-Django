323fb81cb7b05a9f764db46c6d5f0f2efcdcd526
Yasritha <114985002+Yasritha@users.noreply.github.com>
2024-10-09 21:11:30 +0530

    Delete fsdproject directory

--- fsdproject/asgi.py
+++
@@ -1,7 +0,0 @@
-# asgi.py
-import os
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fsdproject.settings')
-
-application = get_asgi_application()
